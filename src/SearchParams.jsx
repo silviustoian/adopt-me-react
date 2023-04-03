441e3cc77a7aefca0e1ef7d4dbb004f5e8f8f864
Silviu Stoian <silviustoian@Silvius-MacBook-Pro.local>
2023-04-03 16:30:53 +0300

    hooks

---
+++ src/SearchParams.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+const SearchParams = () => {
+  let [location, setLocation] = useState("");
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input onChange={(e) => setLocation(e.target.value)} id="location" value={location} placeholder="Location" />
+        </label>
+        <button> Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
